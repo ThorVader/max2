3aa0ea2d33c088c73f1127efb3f7bed9a38bde2c
Tyson Madisa <tysonmadisa@gmail.com>
2023-03-21 10:39:44 -0700

    commit 2

--- App.js
+++ App.js
@@ -1,22 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react'; 
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app.
-        Testing new commit..com
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const[enteredGoalText, setenteredGoalText]= useState(''); //use state left blank coz we dont have it yet. Its user input.
+  
+  const [enteredGoals, setEnteredGoals] = useState( [] ); //used to collect the list of goals/user input.
+
+function goalInputHandler(enteredText) {
+   setenteredGoalText(enteredText);
+};// collecting user input as the user types
+
+function addGoalHandler() {
+  setEnteredGoals((currentEnteredGoals) => [
+    ...currentEnteredGoals, enteredGoalText,]
+)}; //fired when the button is clicked
+
+return (
+  <View style={styles.container}>
+   <View style= {styles.inputContainer}>
+    <TextInput style= {styles.takeText}placeholder='Your course goal.'
+    onChangeText={goalInputHandler}>
+      
+    </TextInput>
+    <Button title='Add goal' onPress={addGoalHandler}></Button>
+
+     </View>
+   <View>
+     {enteredGoals.map((goal) => <Text style={styles.goalItem} key={goal}>{goal} </Text> )}
+   </View>
+  </View>
+);
+} 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   padding: 50
   },
-});
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    paddingBottom: "10%",
+    borderTopWidthnp: 1 
+  },
+  takeText: {
+    borderWidth: 1,
+    borderEndColor: "black",
+    width: '80%',
+    marginRight: "5%"
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: 'white', 
+  }
+  });
